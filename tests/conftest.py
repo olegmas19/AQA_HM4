63a3eaa4fbfa000272a93aac97572a4cb7b318cd
Oleg Maslennikov <olegmas19@mail.ru>
2025-03-02 19:37:50 +0300

    HM_mid-level-step-objects

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from selene import browser
 from selenium import webdriver
 
 
-
 @pytest.fixture(scope="function", autouse=True)
 def browser_open():
     driver_options = webdriver.ChromeOptions()
@@ -11,5 +10,11 @@ def browser_open():
     browser.config.driver_options = driver_options
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
     browser.driver.set_window_size(1700, 1280)
+
     yield
     browser.quit()
+
+
+def removing_banner():
+    browser.driver.execute_script("$('#fixedban').remove()")
+    browser.driver.execute_script("$('footer').remove()")
--- tests/test_demoqa_filling_sending_forms.py
+++ tests/test_demoqa_filling_sending_forms.py
@@ -1,57 +1,34 @@
-from datetime import datetime
-import os
-from selene import browser, have, be
+from aqa_hm4_demoqa_tests.registration_page import RegistrationPage
 
 
 def test_filling_sending_forms():
-    browser.open('/')
-    # Для мест, где есть баннеры.
-    browser.driver.execute_script("$('#fixedban').remove()")
-    browser.driver.execute_script("$('footer').remove()")
-    browser.should(have.title('DEMOQA'))
+    registration_page = RegistrationPage()
+    # GIVEN
+    registration_page.open()
 
-    browser.element('#firstName').should(be.blank).type('Иван')
-    browser.element('#lastName').should(be.blank).type('Иванов')
-    browser.element('#userEmail').should(be.blank).type('Ivan@mail.ru')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').should(be.blank).type('8989567666')
-    browser.element('#dateOfBirthInput').should(have.value(datetime.now().strftime('%d %b %Y'))).click()
-    browser.element('.react-datepicker__month-select').element('[value="1"]').should(have.exact_text('February')).click()
-    browser.element('.react-datepicker__year-select').element('[value="1992"]').should(have.exact_text('1992')).click()
-    browser.all('.react-datepicker__week')[4].all('[role="option"]')[6].should(have.exact_text('29')).click()
-    browser.element('#subjectsInput').type('History').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('../data/IMG_1332.JPEG'))
-    browser.element('#currentAddress').should(be.blank).type('Россия, г. Сызрань, Солнечная ул., д. 12 кв.137')
-    browser.element('#state').click().element('#react-select-3-option-0').click()
-    browser.element('#city').click().element('#react-select-4-option-0').click()
-    browser.element('#submit').click()
+    # WHEN
+    registration_page.fill_first_name('Иван')
+    registration_page.fill_last_name('Иванов')
+    registration_page.fill_email('Ivan@mail.ru')
+    registration_page.fill_gender_male()
+    registration_page.fill_phonenumber('8989567666')
+    registration_page.fill_dateOfBirth('February', '29', '1992')
+    registration_page.fill_subject('History')
+    registration_page.fill_hobbies('Sports', 'Reading', 'Music')
+    registration_page.fill_attachment('IMG_1332.JPEG')
+    registration_page.fill_address('Солнечная ул., д. 12 кв.137', 'Haryana', 'Karnal')
+    registration_page.fill_submit()
 
-
-    browser.element('.table').should(have.text('Иван Иванов'))
-    browser.element('.table').should(have.text('Ivan@mail.ru'))
-    browser.element('.table').should(have.text('Male'))
-    browser.element('.table').should(have.text('8989567666'))
-    browser.element('.table').should(have.text('29 February,1992'))
-    browser.element('.table').should(have.text('History'))
-    browser.element('.table').should(have.text('Sports, Reading, Music'))
-    browser.element('.table').should(have.text('IMG_1332.JPEG'))
-    browser.element('.table').should(have.text('Россия, г. Сызрань, Солнечная ул., д. 12 кв.137'))
-    browser.element('.table').should(have.text('NCR Delhi'))
-    browser.element('#closeLargeModal').click()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # THEN
+    registration_page.should_have_registered(
+        'Иван Иванов',
+        'Ivan@mail.ru',
+        'Male',
+        '8989567666',
+        '29 February,1992',
+        'History',
+        'Sports, Reading, Music',
+        'IMG_1332.JPEG',
+        'Солнечная ул., д. 12 кв.137',
+        'Haryana Karnal',
+    )
