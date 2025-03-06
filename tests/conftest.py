9f70f8d99afc03a91cefe420cec2391ba9511952
Oleg Maslennikov <olegmas19@mail.ru>
2025-03-06 22:03:43 +0300

    HM_test_branch_v1_light_v2.6 Доработал Conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
-# import pytest
-# from selene import browser
-# from selenium import webdriver
+import pytest
+from requests import options
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+
 #
 #
 # @pytest.fixture(scope="function", autouse=True)
@@ -18,3 +23,23 @@
 # def removing_banner():
 #     browser.driver.execute_script("$('#fixedban').remove()")
 #     browser.driver.execute_script("$('footer').remove()")
+
+
+@pytest.fixture(scope="function")
+def setup_open(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "125.0",
+        "selenoid:options": {"enableVideo": True, "enabledVNC": True},
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options,
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+    browser.quit()
