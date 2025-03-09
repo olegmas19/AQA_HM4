4c7c206965983ed27274fa6bcf075a4062b20329
Oleg Maslennikov <olegmas19@mail.ru>
2025-03-09 15:45:30 +0300

    HM_temporary-mid-level-step-objects_v1.5

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,20 +2,31 @@ import pytest
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 from utils import attach
+from dotenv import load_dotenv
+import os
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
+
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться, но html загрузился
+    browser.config.driver_options = driver_options
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
-    # driver_options.add_argument('--headless')
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -25,7 +36,7 @@ def setup_browser():
 
     options.capabilities.update(selenoid_capabilities)
     browser.config.driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
         options=options,
     )
 
@@ -35,25 +46,3 @@ def setup_browser():
     attach.add_logs(browser)
     attach.add_video(browser)
     browser.quit()
-
-
-# import pytest
-# from selene import browser
-# from selenium import webdriver
-#
-#
-# @pytest.fixture(scope="function", autouse=True)
-# def browser_open():
-#     driver_options = webdriver.ChromeOptions()
-#     driver_options.page_load_strategy = 'eager'
-#     browser.config.driver_options = driver_options
-#     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-#     browser.driver.set_window_size(1700, 1280)
-#
-#     yield
-#     browser.quit()
-#
-#
-# def removing_banner():
-#     browser.driver.execute_script("$('#fixedban').remove()")
-#     browser.driver.execute_script("$('footer').remove()")
