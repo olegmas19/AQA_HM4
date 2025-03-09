5868dcefe8918d593388e91a4d8c1a73f00dd6ae
Oleg Maslennikov <olegmas19@mail.ru>
2025-03-09 14:03:10 +0300

    HM_temporary-mid-level-step-objects_v1.0

--- aqa_hm4_demoqa_tests/resourse.py
+++ aqa_hm4_demoqa_tests/resourse.py
@@ -3,10 +3,3 @@ import os
 CURRENT_FILE = os.path.abspath(__file__)
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)
 DATA_DIR = os.path.join(os.path.dirname(CURRENT_DIR), "data")
-
-
-# TMP_DIR = os.path.join(CURRENT_DIR, "data")
-
-# def path(file_name):
-# return
-# os.path.abspath(f'../data/{file_name}')
